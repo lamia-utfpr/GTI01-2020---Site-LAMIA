2314719731584218bdd5c6542c981fcd2b68b654
Jece Xavier <jece.pereira@etec.sp.gov.br>
2020-12-25 15:33:46 -0300

    Configurando padrões de projeto

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,12 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 
-
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
-	console.log(`...... Started on port ${process.env.PORT}!`);
+  console.log(`...... Started on port ${process.env.PORT}!`);
 });
