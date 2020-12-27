6a8f86fcaec2b6c7725035205a36118d95e61518
Jece Xavier <jece.pereira@etec.sp.gov.br>
2020-12-27 14:35:10 -0300

    Criação do HashProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,19 @@
 import { container } from 'tsyringe';
 
-// import './providers';
+import './providers';
 
 import IPatentRepository from '@modules/Patent/repositories/IPatentRepository';
 import PatentRepository from '@modules/Patent/infra/typeorm/repositories/PatentRepository';
 
+import MemberRepository from '@modules/Member/infra/typeorm/repositories/MemberRepository';
+import IMemberRepository from '@modules/Member/repositories/IMemberRepository';
+
 container.registerSingleton<IPatentRepository>(
   'PatentRepository',
   PatentRepository,
+);
+
+container.registerSingleton<IMemberRepository>(
+  'MemberRepository',
+  MemberRepository,
 );
